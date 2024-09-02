e766d02b36838dba13560d183be41c57c0b32453
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-09-02 18:40:35 +0400

    fix: use static value istead of magic string in theme toggler component

--- src/components/theme-toggler/theme-toggler.jsx
+++ src/components/theme-toggler/theme-toggler.jsx
@@ -3,16 +3,18 @@
 import React, { useContext } from "react";
 import styles from "./theme-toggler.module.css";
 import { ThemeContext } from "@/context/theme.context";
+import { THEME_MODE } from "@/constants";
 
 const ThemeToggler = () => {
   const { toggle, mode } = useContext(ThemeContext);
+
   return (
     <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
       <div
         className={styles.ball}
-        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+        style={mode === THEME_MODE.LIGHT ? { left: "2px" } : { right: "2px" }}
       />
     </div>
   );
