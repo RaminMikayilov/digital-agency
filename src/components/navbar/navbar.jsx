1736e71aa1d023aee476a08feab9df80c833d4a2
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-09-10 18:41:53 +0400

    feat: add signout functionality

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 import { links } from "./constants";
 import styles from "./navbar.module.css";
 import ThemeToggler from "../theme-toggler/theme-toggler";
+import { signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
-  const signOut = () => {
-    console.log("Sign out");
-  };
+  const session = useSession();
+
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
@@ -21,10 +21,11 @@ const Navbar = () => {
             {link.title}
           </Link>
         ))}
-
-        <button className={styles.logout} onClick={signOut}>
-          Logout
-        </button>
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
