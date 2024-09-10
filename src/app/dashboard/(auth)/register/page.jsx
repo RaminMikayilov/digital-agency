256e81ffc0786241f88cf85dbc23e35653fc88e8
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-09-10 18:37:43 +0400

    feat: implement authentication

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,5 +1,69 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./page.module.css";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [error, setError] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const [name, email, password] = e.target.elements;
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: name.value,
+          email: email.value,
+          password: password.value,
+        }),
+      });
+
+      res.status === 201 &&
+        router.push("/dashboard/login?success=Account has been created");
+    } catch (error) {
+      setError(true);
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Username"
+          required
+          className={styles.input}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+        {error && "Something went wrong!"}
+      </form>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/login">
+        Login with an existing account
+      </Link>
+    </div>
+  );
 };
 
 export default Register;
