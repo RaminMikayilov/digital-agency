9020e4c88d94a49e8e436f3c61eeda3b8d38b202
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-09-10 18:45:58 +0400

    chore: implement protector for route

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const session = useSession();
+  const router = useRouter();
+
+  if (session.status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (session.status !== "authenticated") {
+    router.push("/dashboard/login");
+  }
+
+  if (session.status === "authenticated") {
+    return <div>Dashboard</div>;
+  }
 };
 
 export default Dashboard;
